1bade1d80e653a2ab45c829439cbd681e14b17fb
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-04-18 21:29:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,1 @@
-$.ajaxSetup({
-  headers : {
-    'Authorization' : 'Bot',
-    'Content-Type' : 'application/json'
-  }
-});
-
-
-$.getJSON("https://discordapp.com/api/users/451707918320926733",
-	function(data){
-		console.log(data);
-
-		var avatar = "https://cdn.discordapp.com/avatars/451707918320926733/" + data.avatar;
-		$(".avatar").attr("src", avatar);
-	}
-);
+var TOKEN = "Hahaha"
