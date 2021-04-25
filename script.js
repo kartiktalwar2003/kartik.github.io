5cff31d19db767352fbb5b471be2df9a5edaf8e1
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-04-26 01:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+$.getJSON("https://kartik-avatar.herokuapp.com",
+	function(data){
+		var avatar = "https://cdn.discordapp.com/avatars/451707918320926733/" + data.avatar;
+		$(".avatar").attr("src", avatar);
+	}
+);
