c9bce11005f906c3b22645236e25db743040ed0e
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2022-05-07 18:41:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $.getJSON("https://lazybuds.herokuapp.com/api",
 		var avatar = "https://cdn.discordapp.com/avatars/451707918320926733/" + data.avatar;
 		$(".avatar").attr("src", avatar);
 
-    var headingElement = $(".heading");
-    headingElement.text(data.link);
+    //var headingElement = $(".heading");
+    //headingElement.text(data.link);
 	}
 );
